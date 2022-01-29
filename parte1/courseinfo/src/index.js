86b7feb1d1138d72c340b65779eb859f78f2bbfb
santi19999 <santiherrera1999@gmail.com>
2022-01-28 22:59:01 -0300

    listo

--- parte1/courseinfo/src/index.js
+++ parte1/courseinfo/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+const Title = ({ course }) => <h1>{course}</h1>;
+const Exercises = ({ part, exercises }) => (
+	<p>
+		{part} {exercises}
+	</p>
+);
+const Final = ({ part1, part2, part3 }) => {
+	return (
+		<p>
+			{part1} {part2} {part3}
+		</p>
+	);
+};
 const App = () => {
 	const course = 'Half Stack application development';
-	const part1 = 'Fundamentals of React';
+	const part1 = 'Fundamentals of  React';
 	const exercises1 = 10;
 	const part2 = 'Using props to pass data';
 	const exercises2 = 7;
@@ -12,17 +24,11 @@ const App = () => {
 
 	return (
 		<div>
-			<h1>{course}</h1>
-			<p>
-				{part1} {exercises1}
-			</p>
-			<p>
-				{part2} {exercises2}
-			</p>
-			<p>
-				{part3} {exercises3}
-			</p>
-			<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+			<Title course={course} />
+			<Exercises part={part1} exercises={exercises1} />
+			<Exercises part={part2} exercises={exercises2} />
+			<Exercises part={part3} exercises={exercises3} />
+			Number of exercises <Final part1={part1} part2={part2} part3={part3} />
 		</div>
 	);
 };
